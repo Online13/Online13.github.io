7592ffec640afa964278e5c3c7466e301eb794e4
RATIARIVELO Nekena Rayane <rratiarivelo@gmail.com>
2025-01-31 07:24:18 +0300

    fix build

--- src/components/molecules/ProjectPreview.tsx
+++ src/components/molecules/ProjectPreview.tsx
@@ -9,24 +9,10 @@ type ProjectPreviewProps = {
 };
 
 export function ProjectPreview({
-	title,
-	subtitle,
 	preview,
 }: ProjectPreviewProps) {
 	return (
 		<motion.div className="group border-b border-b-border cursor-pointer duration-500 rounded-md mt-4">
-			{/* <motion.div className="w-full pt-4 pb-4 z-30 space-y-2 flex items-start justify-between">
-				<div className="space-y-4">
-					<div className="transition-[padding] px-6">
-						<h2 className="text-xl lg:text-2xl font-semibold text-[#002AFF]">
-							{title}
-						</h2>
-						<h2 className="text-xl lg:text-2xl font-semibold max-w-md">
-							{subtitle}
-						</h2>
-					</div>
-				</div>
-			</motion.div> */}
 			<div
 				className={cn(
 					"w-full h-[420px] border border-border hover:border-primary rounded-xl overflow-hidden z-20 flex justify-center items-center relative"
